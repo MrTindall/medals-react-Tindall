5c68e3b7e81c474f130b9fdb1ff4269be3af49f5
Alex <tindall.al@gmail.com>
2025-01-27 17:44:05 -0600

    assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Country from "./components/Country";
 import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([
+    { id: 1, name: 'United States', gold: 2 },
+    { id: 2, name: 'China', gold: 3 },
+    { id: 3, name: 'France', gold: 0 },
+  ]);
+
+  function handleDelete(countryId) {
+    console.log(`delete word: ${countryId}`);
+    setCountries(countries.filter((c) => c.id !== countryId));
+  }
+
   return (
     <div>
       <header className="app-header" >Medals</header>
-      <Country />
+      {countries.map((country) => (
+        <Country key={country.id} country={country} onDelete={handleDelete} />
+      ))}
     </div>
   );
 }
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,38 +1,16 @@
-import React, { useState } from "react";
 
-function Country() {
-    const[name, setName] = useState("United States")
-    const [goldMedals, setGoldMedals] = useState(0);
 
-    function handleClick() {
-        setGoldMedals(goldMedals + 1);
-    }
-
+export default function Country(props) {
     return (
         <div
             className="country"
-            style={{
-                border: `solid 2px red`,
-                borderRadius: `8px`,
-                padding: `10px`,
-            }}
         >
-            <div style={{ padding: `10px` }}>
-                <label htmlFor="gold-medals" style={{ paddingRight: `10px` }}>
-                    {name}
-                </label>
-                <input
-                    type="number"
-                    name="gold-medals"
-                    id="gold"
-                    style={{ width: `100px`, textAlign: "center" }}
-                    value={goldMedals}
-                />
+            <div>
+                <h3>{props.country.name}</h3>
+                <h4>Gold Medals: {props.country.gold}</h4>
             </div>
 
-            <button onClick={handleClick}>Click me</button>
+            <button onClick={() => props.onDelete(props.country.id)}>Delete</button>
         </div>
     );
 }
-
-export default Country;
